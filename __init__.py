636dcddaa6e88cb9abbfbbbc212c68af8e9be4a1
MANOJKUMARKADIYAM <manojkumarkadiyam94@gmail.com>
2026-02-23 13:10:59 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+
+# Make the application module available when the package is imported
+import emotion_detection
+
+# Optionally expose key functions at package level
+# from emotion_detection import emotion_detector, format_emotion_result
